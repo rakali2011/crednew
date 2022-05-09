3063c132e28b08fdda58eb6e13a1e2c9b6307782
rakali2011 <rakali2011@gmail.com>
2022-05-10 00:30:51 +0500

    updated code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,6 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ config('app.name', 'Laravel') }}</title>
-  
 
   <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
